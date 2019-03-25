7d37383ecf24406e706c5e13dabd2462d850c178
P D K Pavan Kumar <pavankumar8545@gmail.com>
2019-03-25 16:27:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/**import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
@@ -13,6 +13,31 @@ const App = () => {
             <CommentDetail author="nam" timeAgo="Yesterday at 6.00PM" bodyText="how afsdfdsfdsf" imageSrc={faker.image.avatar()} />
         </div>
     );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root')); **/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { latitude: null, errMsg: '' };
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({ latitude: position.coords.latitude });
+    }, (error) => {
+      this.setState({ errMsg: error.message });
+    });
+  }
+  render() {
+    if (this.state.errMsg && !this.state.latitude) {
+      return (<div>Error: {this.state.errMsg} </div>);
+    }
+    if (!this.state.errMsg && this.state.latitude) {
+      return (<div>Latitude: {this.state.latitude}</div>)
+    }
+    return (<div>Loading...</div>);
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
